9ed9aedaf7a60d53cddf3ccbbc8dd611d4386760
timorichert <timo.richert@pvyield.com>
2018-10-29 19:12:23 +0100

    Various changed, implemented Flask/Rest tutorial. Separated Model/Service/Resource. Implemented versioning.

---
+++ api/v1/__init__.py
@@ -0,0 +1,39 @@
+from flask_restplus import Api
+from flask import Blueprint
+
+from .main.resource.user import api as user_ns
+from .main.resource.plantspec import api as plantspec_ns
+from .main.resource.meteodata import api as meteodata_ns
+from .main.resource.economics import api as economics_ns
+from .main.resource.simulation import api as simulation_ns
+from .main.resource.analysis import api as analysis_ns
+
+blueprint = Blueprint('api', __name__)
+
+api = Api(blueprint,
+          version='0.1(prototype)',
+          title='pvyield API',
+          description='<table><tr>'
+                      '<td>'
+                      'Development <i><u>prototype</u></i> of the <b>pvyield RESTful API service</b>.<br />'
+                      'This API provides a complete cloud-based toolchain for the performance analysis of large-scale, '
+                      'solar photovoltaic power plants and allows to:'
+                      '<ul><li>Define a <b>plant configuration</b> including main components,</li>'
+                      '<li>Create and use <b>irradiance datasets</b> based on used-specified hourly data or datasets from commercial vendors,</li>' 
+                      '<li>Optionally set <b>financial boundary conditions</b> for the overall analysis,</li>'
+                      '<li><b>Run performance simulations</b> (based on <a href="https://www.nrel.gov/">NREL</a>-driven, open-source <a href="https://github.com/NREL/ssc">SAM</a>),</li>'
+                      '<li><b>Optimize</b> for LCOE, IRR, or NPV with <a href="https://en.wikipedia.org/wiki/Genetic_algorithm">genetic optimization</a>, and finally to</li>'
+                      '<li><b>Analyze results</b> via technical and economic statistical analysis (P90, P50, P10, etc.)</li></ul>'
+                      'Feel free to <a href="mailto:info@pvyield.com">get in touch</a> if you are interested in this project.'
+                      '</td>'
+                      '<td style="text-align:right"><img src="http://external.pvyield.com/logo_02_darkblue_large.svg" alt="logo" style="width:250px;height:130px" /></td>'
+                      '</tr></table>',
+          contact='info@pvyield.com',
+          contact_url='https://pvyield.com')
+
+api.add_namespace(plantspec_ns, path='/plantspec')
+api.add_namespace(meteodata_ns, path='/meteodata')
+api.add_namespace(economics_ns, path='/economics')
+api.add_namespace(simulation_ns, path='/simulation')
+api.add_namespace(analysis_ns, path='/analysis')
+api.add_namespace(user_ns, path='/user')
---
+++ manage.py
@@ -0,0 +1,49 @@
+import os
+import unittest
+from flask import redirect
+from flask_jwt import JWT
+# from __security import authenticate, identity
+
+# from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+from api.v1.main import create_app
+from api.v1 import blueprint
+
+# call the create_app function we created initially to create the application instance with the required parameter
+# from the environment variable which can be either of the following - dev, prod, test.
+# If none is set in the environment variable, the default dev is used.
+
+app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
+app.register_blueprint(blueprint, url_prefix='/v1')
+
+app.app_context().push()
+manager = Manager(app)
+# jwt = JWT(app, authenticate, identity)
+
+#migrate = Migrate(api, db)
+#manager.add_command('db', MigrateCommand)
+
+
+@manager.command  # marks function as executable from the command line.
+def run():
+    app.run()
+
+
+@manager.command
+def test():
+    """Runs the unit tests."""
+    tests = unittest.TestLoader().discover('api/test', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
+
+@app.route("/")
+def index():
+    return redirect('/v1/')
+
+
+if __name__ == '__main__':
+    manager.run()
